e7e97b97cfea71dc19bcad81c0c6cee2550a4d53
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-06-09 18:22:14 -0400

    mobile responsive

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -12,7 +12,7 @@ const Portfolio = () => {
         setCovid(false)
         setFashun(false)
         setCalorie(false)
-        window.scrollTo(0, 500)
+        window.scrollTo(0, 850)
     }
 
     const covidModal = () => {
@@ -20,7 +20,7 @@ const Portfolio = () => {
         setNetflastinate(false)
         setFashun(false)
         setCalorie(false)
-        window.scrollTo(0, 500)
+        window.scrollTo(0, 850)
     }
 
     const fashunModal = () => {
@@ -28,7 +28,7 @@ const Portfolio = () => {
         setCovid(false)
         setNetflastinate(false)
         setCalorie(false)
-        window.scrollTo(0, 500)
+        window.scrollTo(0, 850)
     }
 
     const caloriModal = () => {
@@ -36,7 +36,7 @@ const Portfolio = () => {
         setCovid(false)
         setNetflastinate(false)
         setFashun(false)
-        window.scrollTo(0, 500)
+        window.scrollTo(0, 850)
     }
 
     return (
@@ -45,23 +45,23 @@ const Portfolio = () => {
 
         <div className="left">
                 <img src="https://i.ibb.co/pWs82w7/netflastinat.png" 
-                    style={{width:"400px", height:"100px"}}
+                    style={{width:"385px", height:"100px"}}
                     onClick={netflModal} 
                 />
                 <img src="https://i.ibb.co/vsk4hbr/covid.png"
-                     style={{width:"400px", height:"100px"}} 
+                     style={{width:"385px", height:"100px"}} 
                      onClick={covidModal}/>
             </div>
 
             <div className="center">
                 <img src="https://i.ibb.co/61mSyWk/fashun.png"
-                     style={{width:"355px" ,height:"220px"}} 
+                     style={{width:"385px" ,height:"220px"}} 
                      onClick={fashunModal}/>
             </div>
 
             <div className="right">
                 <img src="https://i.ibb.co/8gKq5P9/calorie.png"
-                     style={{width:"400px", height:"220px"}} 
+                     style={{width:"385px", height:"220px"}} 
                      onClick={caloriModal}/>
             </div>
         </div>
@@ -71,7 +71,7 @@ const Portfolio = () => {
                 <div className="youtubeContainer">
                     <div className="title">
                         <img src="https://i.ibb.co/pWs82w7/netflastinat.png" 
-                            style={{width:"400px", height:"220px"}} />
+                            style={{width:"385px", height:"220px"}} />
                         <div className="icon">
                             <a href="https://github.com/Chul0/Netflastinate-frontend">
                                 <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
@@ -84,7 +84,7 @@ const Portfolio = () => {
                             </a>
                         </div>
                     </div>
-                    <iframe width="780" height="350" src="https://www.youtube.com/embed/il_xlQ79nyk" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe src="https://www.youtube.com/embed/il_xlQ79nyk" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
                 <div className="description">
                     <p>Netflastinate is a movie trailer app where user can watch movie trailers, and save them to watch list.
@@ -98,7 +98,7 @@ const Portfolio = () => {
                  <div className="youtubeContainer">
                     <div className="title">
                         <img src="https://i.ibb.co/vsk4hbr/covid.png" 
-                            style={{width:"400px", height:"220px"}} />
+                            style={{width:"385px", height:"220px"}} />
                         <div className="icon">
                             <a href="https://github.com/Chul0/beat-covid">
                                 <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
@@ -111,9 +111,11 @@ const Portfolio = () => {
                             </a>
                         </div>
                     </div>
-                    <iframe width="780" height="350" src="https://www.youtube.com/embed/7l0plZNASC4" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe src="https://www.youtube.com/embed/7l0plZNASC4" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
+                <div className="description">
                 <p>Beat COVID: user kills coronavirus by clicking viruses.</p>
+                </div>
             </div>
         )}    
 
@@ -123,7 +125,7 @@ const Portfolio = () => {
                 <div className="youtubeContainer">
                     <div className="title">
                         <img src="https://i.ibb.co/MRNjds9/Screen-Shot-2021-06-08-at-9-30-02-PM.png" 
-                            style={{width:"400px", height:"220px"}} />
+                            style={{width:"385px", height:"220px"}} />
                         <div className="icon">
                             <a href="https://github.com/Chul0/fash-un-frontend">
                                 <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
@@ -136,9 +138,11 @@ const Portfolio = () => {
                             </a>
                         </div>
                     </div>
-                    <iframe width="780" height="350" src="https://www.youtube.com/embed/aTrz__8UJ5I" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe src="https://www.youtube.com/embed/aTrz__8UJ5I" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
+                <div className="description">
                 <p>Fash-un app is a fashion show coverage where you can explore runway shows, and create mood boards to save inspiring looks.</p>
+                </div>
             </div>
                 )}
 
@@ -147,7 +151,7 @@ const Portfolio = () => {
                  <div className="youtubeContainer">
                     <div className="title">
                         <img src="https://i.ibb.co/brcBcWN/calorie-2.png" 
-                            style={{width:"400px", height:"220px"}} />
+                            style={{width:"385px", height:"220px"}} />
                         <div className="icon">
                             <a href="https://github.com/Chul0/Calorie-Tracker-FE">
                                 <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
@@ -160,9 +164,11 @@ const Portfolio = () => {
                             </a>
                         </div>
                     </div>
-                    <iframe width="780" height="350" src="https://www.youtube.com/embed/QSRl5Km5r1o" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe src="https://www.youtube.com/embed/QSRl5Km5r1o" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
+                <div className="description">
                     <p>Calorie tracker is a nutrition app that helps users to keep track of their daily calorie intake.</p>
+                </div>
             </div>
         )}
         </>
