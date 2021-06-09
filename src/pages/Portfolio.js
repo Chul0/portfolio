2a0f200430d450db75c0131fdb1c4a9e75b99842
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-06-09 03:57:24 -0400

    portfolio page 90% done

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -8,28 +8,28 @@ const Portfolio = () => {
     const [calorie, setCalorie] = useState(false)
 
     const netflModal =() => {
-        setNetflastinate(!netflastinate)
+        setNetflastinate(true)
         setCovid(false)
         setFashun(false)
         setCalorie(false)
     }
 
     const covidModal = () => {
-        setCovid(!covid)
+        setCovid(true)
         setNetflastinate(false)
         setFashun(false)
         setCalorie(false)
     }
 
     const fashunModal = () => {
-        setFashun(!fashun)
+        setFashun(true)
         setCovid(false)
         setNetflastinate(false)
         setCalorie(false)
     }
 
     const caloriModal = () => {
-        setCalorie(!calorie)
+        setCalorie(true)
         setCovid(false)
         setNetflastinate(false)
         setFashun(false)
@@ -64,72 +64,101 @@ const Portfolio = () => {
        
         {netflastinate && (
             <div className="netflastinate" >
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/il_xlQ79nyk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <p>Netflastinate is a movie trailer app where user can watch movie trailers, and save them to watch list.
-                "Netflastinate" is a slang term used when you use Netflix to procrastinate.</p>
-                <a href="https://github.com/Chul0/Netflastinate-frontend">
-                    <i class="devicon-github-original colored" style={{fontSize:"50px"}} ></i>
-                    <p style={{color:"black"}}>FrontEnd</p>
-                </a>
-                <a href="https://github.com/Chul0/Netflastinate-backend">
-                    <i class="devicon-github-original" style={{color:"grey", fontSize:"50px"}} ></i>
-                    <p style={{color:"black"}}>BackEnd</p>
-                </a>
-                <a href="https://netflastinate.herokuapp.com/">
-                    <p style={{color:"black"}}>Website</p>
-                </a>
+                <div className="youtubeContainer">
+                    <div className="title">
+                        <img src="https://i.ibb.co/pWs82w7/netflastinat.png" 
+                            style={{width:"400px", height:"270px"}} />
+                        <div className="icon">
+                            <a href="https://github.com/Chul0/Netflastinate-frontend">
+                                <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
+                            </a>
+                            <a href="https://github.com/Chul0/Netflastinate-backend">
+                                <img src="https://i.ibb.co/B6KDNWd/be.jpg"/>
+                            </a>
+                            <a href="https://netflastinate.herokuapp.com/">
+                                <img src="https://i.ibb.co/N15ypqp/hp.jpg"/>
+                            </a>
+                        </div>
+                    </div>
+                    <iframe width="780" height="400" src="https://www.youtube.com/embed/il_xlQ79nyk" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div className="description">
+                    <p>Netflastinate is a movie trailer app where user can watch movie trailers, and save them to watch list.
+                    "Netflastinate" is a slang term used when you use Netflix to procrastinate.</p>
+                </div>
             </div>
             )}          
 
         {covid && (
             <div className="covid">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/7l0plZNASC4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                 <div className="youtubeContainer">
+                    <div className="title">
+                        <img src="https://i.ibb.co/vsk4hbr/covid.png" 
+                            style={{width:"400px", height:"270px"}} />
+                        <div className="icon">
+                            <a href="https://github.com/Chul0/beat-covid">
+                                <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
+                            </a>
+                            <a href="https://github.com/Chul0/beat-covid">
+                                <img src="https://i.ibb.co/B6KDNWd/be.jpg"/>
+                            </a>
+                            <a href="https://chul0.github.io/beat-covid/">
+                                <img src="https://i.ibb.co/N15ypqp/hp.jpg"/>
+                            </a>
+                        </div>
+                    </div>
+                    <iframe width="780" height="400" src="https://www.youtube.com/embed/7l0plZNASC4" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
                 <p>Beat COVID: user kills coronavirus by clicking viruses.</p>
-                <a href="https://github.com/Chul0/beat-covid">
-                    <i class="devicon-github-original colored" style={{fontSize:"50px"}} ></i>
-                    <p style={{color:"black"}}>GitHub</p>
-                </a>
-                <a href="https://chul0.github.io/beat-covid/">
-                    <p style={{color:"black"}}>Website</p>
-                </a>
             </div>
         )}    
 
        {fashun && (
            <div
            className="fashun">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/aTrz__8UJ5I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <div className="youtubeContainer">
+                    <div className="title">
+                        <img src="https://i.ibb.co/MRNjds9/Screen-Shot-2021-06-08-at-9-30-02-PM.png" 
+                            style={{width:"400px", height:"270px"}} />
+                        <div className="icon">
+                            <a href="https://github.com/Chul0/fash-un-frontend">
+                                <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
+                            </a>
+                            <a href="https://github.com/Chul0/fash-un-backend">
+                                <img src="https://i.ibb.co/B6KDNWd/be.jpg"/>
+                            </a>
+                            <a href="https://fash-un-frontend.herokuapp.com/">
+                                <img src="https://i.ibb.co/N15ypqp/hp.jpg"/>
+                            </a>
+                        </div>
+                    </div>
+                    <iframe width="780" height="400" src="https://www.youtube.com/embed/aTrz__8UJ5I" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
                 <p>Fash-un app is a fashion show coverage where you can explore runway shows, and create mood boards to save inspiring looks.</p>
-                <a href="https://github.com/Chul0/fash-un-frontend">
-                    <i class="devicon-github-original colored" style={{fontSize:"50px"}} ></i>
-                    <p style={{color:"black"}}>FrontEnd</p>
-                </a>
-                <a href="https://github.com/Chul0/fash-un-backend">
-                    <i class="devicon-github-original" style={{color:"grey", fontSize:"50px"}} ></i>
-                    <p style={{color:"black"}}>BackEnd</p>
-                </a>
-                <a href="https://fash-un-frontend.herokuapp.com/">
-                    <p style={{color:"black"}}>Website</p>
-                </a>
             </div>
                 )}
 
         {calorie && (
-            <div
-             className="calorie">
-             <iframe width="560" height="315" src="https://www.youtube.com/embed/QSRl5Km5r1o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-             <p>Calorie tracker is an app that helps users to keep track of their daily calorie intake.</p>
-                <a href="https://github.com/Chul0/Calorie-Tracker-FE">
-                    <i class="devicon-github-original colored" style={{fontSize:"50px"}} ></i>
-                    <p style={{color:"black"}}>FrontEnd</p>
-                </a>
-                <a href="https://github.com/Chul0/Calorie-Tracker-BE">
-                    <i class="devicon-github-original" style={{color:"grey", fontSize:"50px"}} ></i>
-                    <p style={{color:"black"}}>BackEnd</p>
-                </a>
-                <a href="https://chul-calorie-tracke-fe.herokuapp.com/">
-                    <p style={{color:"black"}}>Website</p>
-                </a>
+            <div className="calorie">
+                 <div className="youtubeContainer">
+                    <div className="title">
+                        <img src="https://i.ibb.co/brcBcWN/calorie-2.png" 
+                            style={{width:"400px", height:"270px"}} />
+                        <div className="icon">
+                            <a href="https://github.com/Chul0/Calorie-Tracker-FE">
+                                <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
+                            </a>
+                            <a href="https://github.com/Chul0/Calorie-Tracker-BE">
+                                <img src="https://i.ibb.co/B6KDNWd/be.jpg"/>
+                            </a>
+                            <a href="https://chul-calorie-tracke-fe.herokuapp.com/">
+                                <img src="https://i.ibb.co/N15ypqp/hp.jpg"/>
+                            </a>
+                        </div>
+                    </div>
+                    <iframe width="780" height="400" src="https://www.youtube.com/embed/QSRl5Km5r1o" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                    <p>Calorie tracker is a nutrition app that helps users to keep track of their daily calorie intake.</p>
             </div>
         )}
         </>
