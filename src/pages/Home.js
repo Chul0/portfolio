2a5db21b95c5479fc07c83d72c17b468a31dc037
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-06-10 01:48:24 -0400

    mailTo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
                 <div className="socials">
                     <a href="https://www.linkedin.com/in/sungchul--park/"><img src="https://i.ibb.co/xYBwdDC/linkedin.jpg" style={{width:"65px"}}></img></a>
                     <a href="https://github.com/Chul0"><img src="https://i.ibb.co/cJQfz6S/git.jpg" style={{width:"65px"}}></img></a>
-                    <a href="sungchulpark0@gmail.com"><img src="https://i.ibb.co/C12sxdX/email.jpg" style={{width:"65px"}}></img></a>
+                    <a href="mailto:sungchulpark0@gmail.com"><img src="https://i.ibb.co/C12sxdX/email.jpg" style={{width:"65px"}}></img></a>
                     <a href="https://docs.google.com/document/d/1UskRFXI4DO3yx9G9IIMbksDsrsV7yPVp4ICc5lNiHfE/edit?usp=sharing"><img src="https://i.ibb.co/XDWz82p/resume.jpg" style={{width:"65px"}}></img></a>
                 </div>
             </div>
