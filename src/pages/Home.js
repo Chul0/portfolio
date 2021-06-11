2af05b2634968459947260d23e12144d4df99777
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-06-10 21:44:50 -0400

    open a new tab, resume pdf

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import resume from '../images/Sungchul_Park_Resume.pdf'
+
 const Home = () => {
     return(
         <div className="home">
@@ -7,10 +9,10 @@ const Home = () => {
                 <p className="whoAmI">Fullstack Developer</p>
                 <p className="intro">I am a creative software engineer, coming from over 4 years of professional experience in fashion</p>
                 <div className="socials">
-                    <a href="https://www.linkedin.com/in/sungchul--park/"><img src="https://i.ibb.co/xYBwdDC/linkedin.jpg" style={{width:"65px"}}></img></a>
-                    <a href="https://github.com/Chul0"><img src="https://i.ibb.co/cJQfz6S/git.jpg" style={{width:"65px"}}></img></a>
-                    <a href="mailto:sungchulpark0@gmail.com"><img src="https://i.ibb.co/C12sxdX/email.jpg" style={{width:"65px"}}></img></a>
-                    <a href="https://docs.google.com/document/d/1UskRFXI4DO3yx9G9IIMbksDsrsV7yPVp4ICc5lNiHfE/edit?usp=sharing"><img src="https://i.ibb.co/XDWz82p/resume.jpg" style={{width:"65px"}}></img></a>
+                    <a href="https://www.linkedin.com/in/sungchul--park/" target="_blank"><img src="https://i.ibb.co/xYBwdDC/linkedin.jpg" style={{width:"65px"}}></img></a>
+                    <a href="https://github.com/Chul0" target="_blank"><img src="https://i.ibb.co/cJQfz6S/git.jpg" style={{width:"65px"}}></img></a>
+                    <a href="mailto:sungchulpark0@gmail.com" target="_blank"><img src="https://i.ibb.co/C12sxdX/email.jpg" style={{width:"65px"}}></img></a>
+                    <a href={resume} target="_blank"><img src="https://i.ibb.co/XDWz82p/resume.jpg" style={{width:"65px"}}></img></a>
                 </div>
             </div>
         </div>
