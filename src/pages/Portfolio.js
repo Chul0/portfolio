ee1309397f5cd2aa77a9a7df00e7685686824478
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-06-22 22:15:04 -0400

    mobile version margin changed

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -16,7 +16,7 @@ const Portfolio = () => {
         setCovid(false)
         setFashun(false)
         setCalorie(false)
-        window.scrollTo(0, 850)
+        window.scrollTo(0, 500)
     }
 
     const covidModal = () => {
@@ -24,7 +24,7 @@ const Portfolio = () => {
         setNetflastinate(false)
         setFashun(false)
         setCalorie(false)
-        window.scrollTo(0, 850)
+        window.scrollTo(0, 500)
     }
 
     const fashunModal = () => {
@@ -32,7 +32,7 @@ const Portfolio = () => {
         setCovid(false)
         setNetflastinate(false)
         setCalorie(false)
-        window.scrollTo(0, 850)
+        window.scrollTo(0, 500)
     }
 
     const caloriModal = () => {
@@ -40,7 +40,7 @@ const Portfolio = () => {
         setCovid(false)
         setNetflastinate(false)
         setFashun(false)
-        window.scrollTo(0, 850)
+        window.scrollTo(0, 500)
     }
 
     return (
@@ -66,6 +66,20 @@ const Portfolio = () => {
         {netflastinate && (
             <div className="netflastinate" >
                 <div className="youtubeContainer">
+                <div className="title">
+                        <img src="https://i.ibb.co/pWs82w7/netflastinat.png" />
+                        <div className="icon">
+                            <a href="https://github.com/Chul0/Netflastinate-frontend" target="_blank">
+                                <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
+                            </a>
+                            <a href="https://github.com/Chul0/Netflastinate-backend" target="_blank">
+                                <img src="https://i.ibb.co/B6KDNWd/be.jpg"/>
+                            </a>
+                            <a href="https://netflastinate.herokuapp.com/" target="_blank">
+                                <img src="https://i.ibb.co/N15ypqp/hp.jpg"/>
+                            </a>
+                        </div>
+                    </div>
                     <iframe src="https://www.youtube.com/embed/il_xlQ79nyk" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <div className="appDetail">
                     <p className="appName">Netflastinate </p>
@@ -88,6 +102,20 @@ const Portfolio = () => {
         {covid && (
             <div className="covid">
                  <div className="youtubeContainer">
+                 <div className="title">
+                        <img src="https://i.ibb.co/vsk4hbr/covid.png" />
+                        <div className="icon">
+                            <a href="https://github.com/Chul0/beat-covid" target="_blank">
+                                <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
+                            </a>
+                            <a href="https://github.com/Chul0/beat-covid" target="_blank">
+                                <img src="https://i.ibb.co/B6KDNWd/be.jpg"/>
+                            </a>
+                            <a href="https://chul0.github.io/beat-covid/" target="_blank">
+                                <img src="https://i.ibb.co/N15ypqp/hp.jpg"/>
+                            </a>
+                        </div>
+                    </div>
                     <iframe src="https://www.youtube.com/embed/7l0plZNASC4" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     <div className="appDetail">
                         <p className="appName">Beat COVID!</p>
@@ -110,6 +138,20 @@ const Portfolio = () => {
            <div
            className="fashun">
                 <div className="youtubeContainer">
+                <div className="title">
+                        <img src="https://i.ibb.co/4WnNrFf/fashun.png" />
+                        <div className="icon">
+                            <a href="https://github.com/Chul0/fash-un-frontend" target="_blank">
+                                <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
+                            </a>
+                            <a href="https://github.com/Chul0/fash-un-backend" target="_blank">
+                                <img src="https://i.ibb.co/B6KDNWd/be.jpg"/>
+                            </a>
+                            <a href="https://fash-un-frontend.herokuapp.com/" target="_blank">
+                                <img src="https://i.ibb.co/N15ypqp/hp.jpg"/>
+                            </a>
+                        </div>
+                    </div>
                     <iframe src="https://www.youtube.com/embed/aTrz__8UJ5I" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     <div className="appDetail">
                     <p className="appName">Fash-un </p>
@@ -131,6 +173,20 @@ const Portfolio = () => {
         {calorie && (
             <div className="calorie">
                  <div className="youtubeContainer">
+                 <div className="title">
+                        <img src="https://i.ibb.co/YtYQhc7/calorie.png" />
+                        <div className="icon">
+                            <a href="https://github.com/Chul0/Calorie-Tracker-FE" target="_blank">
+                                <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
+                            </a>
+                            <a href="https://github.com/Chul0/Calorie-Tracker-BE" target="_blank">
+                                <img src="https://i.ibb.co/B6KDNWd/be.jpg"/>
+                            </a>
+                            <a href="https://chul-calorie-tracke-fe.herokuapp.com/" target="_blank">
+                                <img src="https://i.ibb.co/N15ypqp/hp.jpg"/>
+                            </a>
+                        </div>
+                    </div>
                     <iframe src="https://www.youtube.com/embed/QSRl5Km5r1o" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     <div className="appDetail">
                     <p className="appName">Calorie tracker </p>
