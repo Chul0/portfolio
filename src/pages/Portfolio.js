379d746d86b2b131424a50fb94ecab2dfbf8005e
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-06-22 21:52:39 -0400

    Portfolio page layout changed

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -66,26 +66,21 @@ const Portfolio = () => {
         {netflastinate && (
             <div className="netflastinate" >
                 <div className="youtubeContainer">
-                    <div className="title">
-                        <img src="https://i.ibb.co/pWs82w7/netflastinat.png" />
-                        <div className="icon">
-                            <a href="https://github.com/Chul0/Netflastinate-frontend" target="_blank">
-                                <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
-                            </a>
-                            <a href="https://github.com/Chul0/Netflastinate-backend" target="_blank">
-                                <img src="https://i.ibb.co/B6KDNWd/be.jpg"/>
-                            </a>
-                            <a href="https://netflastinate.herokuapp.com/" target="_blank">
-                                <img src="https://i.ibb.co/N15ypqp/hp.jpg"/>
-                            </a>
-                        </div>
-                    </div>
                     <iframe src="https://www.youtube.com/embed/il_xlQ79nyk" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-                <div>
+                <div className="appDetail">
+                    <p className="appName">Netflastinate </p>
                     <p className="description">Netflastinate is a movie trailer app where the user can watch movie trailers and save them in their watch-list.
                     "Netflastinate" – (slang) using Netflix to procrastinate</p>
                     <p className="skillUsed">React.js, Express, Node.js, Sequelize, PostgreSQL, CSS, HTML </p>
+                    <div className="sources">
+                        <a href="https://github.com/Chul0/Netflastinate-frontend" target="_blank">Frontend Github
+                        </a>
+                        <a href="https://github.com/Chul0/Netflastinate-backend" target="_blank">Backend Github
+                        </a>
+                        <a href="https://netflastinate.herokuapp.com/" target="_blank">Live Webpage
+                        </a>
+                    </div>
+                </div>
                 </div>
             </div>
             )}          
@@ -93,25 +88,20 @@ const Portfolio = () => {
         {covid && (
             <div className="covid">
                  <div className="youtubeContainer">
-                    <div className="title">
-                        <img src="https://i.ibb.co/vsk4hbr/covid.png" />
-                        <div className="icon">
-                            <a href="https://github.com/Chul0/beat-covid" target="_blank">
-                                <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
+                    <iframe src="https://www.youtube.com/embed/7l0plZNASC4" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div className="appDetail">
+                        <p className="appName">Beat COVID!</p>
+                        <p className="description">Beat COVID! The user kills the COVID-19 virus with a sanitizing click.</p>
+                        <p className="skillUsed">Javascript, CSS, HTML </p>
+                        <div className="sources">
+                            <a href="https://github.com/Chul0/beat-covid" target="_blank">Frontend Github
                             </a>
-                            <a href="https://github.com/Chul0/beat-covid" target="_blank">
-                                <img src="https://i.ibb.co/B6KDNWd/be.jpg"/>
+                            <a href="https://github.com/Chul0/beat-covid" target="_blank">Backend Github
                             </a>
-                            <a href="https://chul0.github.io/beat-covid/" target="_blank">
-                                <img src="https://i.ibb.co/N15ypqp/hp.jpg"/>
+                            <a href="https://chul0.github.io/beat-covid/" target="_blank">Live Webpage
                             </a>
                         </div>
                     </div>
-                    <iframe src="https://www.youtube.com/embed/7l0plZNASC4" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-                <div>
-                <p className="description">Beat COVID! The user kills the COVID-19 virus with a sanitizing click.</p>
-                <p className="skillUsed">Javascript, CSS, HTML </p>
                 </div>
             </div>
         )}    
@@ -120,25 +110,20 @@ const Portfolio = () => {
            <div
            className="fashun">
                 <div className="youtubeContainer">
-                    <div className="title">
-                        <img src="https://i.ibb.co/4WnNrFf/fashun.png" />
-                        <div className="icon">
-                            <a href="https://github.com/Chul0/fash-un-frontend" target="_blank">
-                                <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
-                            </a>
-                            <a href="https://github.com/Chul0/fash-un-backend" target="_blank">
-                                <img src="https://i.ibb.co/B6KDNWd/be.jpg"/>
-                            </a>
-                            <a href="https://fash-un-frontend.herokuapp.com/" target="_blank">
-                                <img src="https://i.ibb.co/N15ypqp/hp.jpg"/>
-                            </a>
-                        </div>
-                    </div>
                     <iframe src="https://www.youtube.com/embed/aTrz__8UJ5I" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div className="appDetail">
+                    <p className="appName">Fash-un </p>
+                    <p className="description">Fashion show coverage with expandable runway shows with the ability for users to create mood boards, saving their most inspiring looks.</p>
+                    <p className="skillUsed">React.js, Express, Node.js, Sequelize, PostgreSQL, CSS, HTML </p>
+                    <div className="sources">
+                        <a href="https://github.com/Chul0/fash-un-frontend" target="_blank">Frontend Github
+                        </a>
+                        <a href="https://github.com/Chul0/fash-un-backend" target="_blank">Backend Github
+                        </a>
+                        <a href="https://fash-un-frontend.herokuapp.com/" target="_blank">Live Webpage
+                        </a>
+                    </div>
                 </div>
-                <div>
-                <p className="description">Fash-un – fashion show coverage with expandable runway shows with the ability for users to create mood boards, saving their most inspiring looks.</p>
-                <p className="skillUsed">React.js, Express, Node.js, Sequelize, PostgreSQL, CSS, HTML </p>
                 </div>
             </div>
                 )}
@@ -146,25 +131,20 @@ const Portfolio = () => {
         {calorie && (
             <div className="calorie">
                  <div className="youtubeContainer">
-                    <div className="title">
-                        <img src="https://i.ibb.co/YtYQhc7/calorie.png" />
-                        <div className="icon">
-                            <a href="https://github.com/Chul0/Calorie-Tracker-FE" target="_blank">
-                                <img src="https://i.ibb.co/0jrq4X8/fe.jpg"/>
-                            </a>
-                            <a href="https://github.com/Chul0/Calorie-Tracker-BE" target="_blank">
-                                <img src="https://i.ibb.co/B6KDNWd/be.jpg"/>
-                            </a>
-                            <a href="https://chul-calorie-tracke-fe.herokuapp.com/" target="_blank">
-                                <img src="https://i.ibb.co/N15ypqp/hp.jpg"/>
-                            </a>
-                        </div>
-                    </div>
                     <iframe src="https://www.youtube.com/embed/QSRl5Km5r1o" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-                <div>
+                    <div className="appDetail">
+                    <p className="appName">Calorie tracker </p>
                     <p className="description">Calorie tracker is a nutrition app that helps users keep track of their daily caloric intake.</p>
                     <p className="skillUsed">Javascript, RESTful API, Express, Node.js, Sequelize, PostgreSQL, CSS3, HTML5 </p>
+                    <div className="sources">
+                        <a href="https://github.com/Chul0/Calorie-Tracker-FE" target="_blank">Frontend Github
+                        </a>
+                        <a href="https://github.com/Chul0/Calorie-Tracker-BE" target="_blank">Backend Github
+                        </a>
+                        <a href="https://chul-calorie-tracke-fe.herokuapp.com/" target="_blank">Live Webpage
+                        </a>
+                    </div>
+                </div>
                 </div>
             </div>
         )}
